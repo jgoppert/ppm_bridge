b789d2ef68e11485a6844823dbc803a3fcc7c654
James Goppert <james.goppert@gmail.com>
2025-11-26 13:26:19 -0500

    Add license to C++ to pass ROS 2 cpplint.
    Signed-off-by: James Goppert <james.goppert@gmail.com>

--- src/ppm_bridge_node.cpp
+++ src/ppm_bridge_node.cpp
@@ -1,3 +1,17 @@
+// Copyright 2025 CogniPilot Foundation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include <algorithm>
 #include <memory>
 
@@ -107,9 +121,8 @@ private:
         m_servo_data.data[3] = a_servo_data.data[3];
         m_servo_data.data[4] = a_servo_data.data[4];
       }
-    }
-    // else, this will work with f310 logitech
-    else {
+    } else {
+      // else, this will work with f310 logitech
       if (msg->axes.size() < 5 || msg->buttons.empty()) {
         RCLCPP_WARN(this->get_logger(), "Logitech mode requires at least 5 axes and 1 button");
         return;
